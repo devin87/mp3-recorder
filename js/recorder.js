47cad18608db083455af467a107c913577c2012e
Devin <devin87@qq.com>
2019-05-23 23:42:55 +0800

    优先使用 MediaRecorder 录音，以解决新版Chrome录音失效的问题
    Signed-off-by: Devin <devin87@qq.com>

--- js/recorder.js
+++ js/recorder.js
@@ -9,7 +9,21 @@
 
     //浏览器兼容
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    //优先使用MediaRecorder
+    var MediaRecorder = window.MediaRecorder;
+
+    //navigator.getUserMedia已被废除，建议使用 navigator.mediaDevices.getUserMedia
+    var mediaDevices = navigator.mediaDevices,
+        getUserMedia = mediaDevices ? mediaDevices.getUserMedia : undefined;
+
+    if (mediaDevices && mediaDevices.getUserMedia) {
+        navigator.getUserMedia = function (ops, success, error) {
+            return mediaDevices.getUserMedia(ops).then(success).catch(error);
+        };
+    } else if (!navigator.getUserMedia) {
+        navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    }
 
     //MP3音频录制
     function MP3Recorder(ops) {
@@ -24,86 +38,105 @@
 
         //录音支持检测
         self.support = !!(window.AudioContext && navigator.getUserMedia && window.Worker);
-
-        self.init();
     }
 
     MP3Recorder.prototype = {
         constructor: MP3Recorder,
 
-        //初始化
-        init: function () {
-            var self = this;
-            if (!self.support) return;
-
-            var ops = self.ops,
-                onComplete = ops.complete,
-                onError = ops.error,
-
-                context = new AudioContext(),
-                worker = new Worker(ops.WORKER_PATH || 'js/recorder-worker.js');
-
-            worker.onmessage = function (e) {
-                var obj = e.data, data = obj.data;
-
-                switch (obj.cmd) {
-                    case "complete":
-                        if (onComplete) onComplete(data, "audio/mp3");
-                        break;
-
-                    case "error":
-                        if (onError) onError(data);
-                        break;
-                }
-            };
-
-            self.inputSampleRate = context.sampleRate;
-            self.outputSampleRate = ops.sampleRate || self.inputSampleRate;
-
-            worker.postMessage({
-                cmd: "init",
-                data: {
-                    numChannels: self.numChannels,
-                    sampleBits: self.sampleBits,
-                    inputSampleRate: self.inputSampleRate,
-                    outputSampleRate: self.outputSampleRate,
-                    bitRate: self.bitRate
-                }
-            });
-
-            self.context = context;
-            self.worker = worker;
-        },
-
         //开始录音
         start: function (success, error) {
             var self = this;
 
-            navigator.getUserMedia({ audio: true }, function (stream) {
-                var context = self.context,
-                    worker = self.worker,
+            if (!self.support) return error && error({ code: "NOT_SUPPORTED_ERROR" });
 
-                    numChannels = self.numChannels,
+            var ops = self.ops,
+                onComplete = ops.complete,
+                onError = ops.error;
 
-                    source = context.createMediaStreamSource(stream),
-                    processor = (context.createScriptProcessor || context.createJavaScriptNode).call(context, self.bufferSize, numChannels, numChannels);
+            navigator.getUserMedia({ audio: true, video: false }, function (stream) {
+                //优先使用 MediaRecorder
+                if (MediaRecorder && ops.useMediaRecorder !== false) {
+                    var recorderType = ops.recorderType;
+                    var recorder = new MediaRecorder(stream, recorderType && MediaRecorder.isTypeSupported(recorderType) ? { mimeType: recorderType } : undefined);
 
-                processor.onaudioprocess = function (e) {
-                    if (self._paused) return;
+                    var data = [];
+                    recorder.addEventListener('dataavailable', function (e) {
+                        if (self._paused) return;
 
-                    var data = [], i = 0;
-                    for (; i < numChannels; i++) {
-                        data.push(e.inputBuffer.getChannelData(i));
-                    }
+                        if (e.data.size > 0) {
+                            data.push(e.data);
+                        }
+                    });
 
-                    worker.postMessage({ cmd: "encode", data: data });
-                };
+                    recorder.addEventListener('stop', function () {
+                        var ops = self.ops,
+                            onComplete = ops.complete,
+                            blob = data[0],
+                            ext = blob ? (blob.type || "").match(/^[^\/]+\/([^;]+);?.*?$/)[1] || "" : "";
 
-                source.connect(processor);
-                processor.connect(context.destination);
+                        if (onComplete) onComplete(blob, ext ? "." + ext : "");
+                    });
 
-                self.source = source;
-                self.processor = processor;
+                    recorder.start();
+
+                    self.recorder = recorder;
+                } else {
+                    var context = new AudioContext(),
+                        worker = new Worker(ops.WORKER_PATH || 'js/recorder-worker.js');
+
+                    worker.onmessage = function (e) {
+                        var obj = e.data, data = obj.data;
+
+                        switch (obj.cmd) {
+                            case "complete":
+                                if (onComplete) onComplete(new Blob(data, { type: "audio/mp3" }), ".mp3");
+                                break;
+
+                            case "error":
+                                if (onError) onError(data);
+                                break;
+                        }
+                    };
+
+                    self.inputSampleRate = context.sampleRate;
+                    self.outputSampleRate = ops.sampleRate || self.inputSampleRate;
+
+                    worker.postMessage({
+                        cmd: "init",
+                        data: {
+                            numChannels: self.numChannels,
+                            sampleBits: self.sampleBits,
+                            inputSampleRate: self.inputSampleRate,
+                            outputSampleRate: self.outputSampleRate,
+                            bitRate: self.bitRate
+                        }
+                    });
+
+                    self.context = context;
+                    self.worker = worker;
+
+                    var numChannels = self.numChannels,
+
+                        source = context.createMediaStreamSource(stream),
+                        processor = (context.createScriptProcessor || context.createJavaScriptNode).call(context, self.bufferSize, numChannels, numChannels);
+
+                    processor.onaudioprocess = function (e) {
+                        if (self._paused) return;
+
+                        var data = [], i = 0;
+                        for (; i < numChannels; i++) {
+                            data.push(e.inputBuffer.getChannelData(i));
+                        }
+
+                        worker.postMessage({ cmd: "encode", data: data });
+                    };
+
+                    source.connect(processor);
+                    processor.connect(context.destination);
+
+                    self.source = source;
+                    self.processor = processor;
+                }
 
                 if (typeof success == "function") success();
 
@@ -120,6 +153,7 @@
         //停止录音
         stop: function () {
             var self = this;
+            if (self.recorder) self.recorder.stop();
             if (self.source) self.source.disconnect();
             if (self.processor) self.processor.disconnect();
             if (self.worker) self.worker.postMessage({ cmd: "stop" });
